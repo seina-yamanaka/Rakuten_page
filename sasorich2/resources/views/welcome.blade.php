cf7f69fc3fe684b0e16677c0560014353b63de56
seina.yamanaka <seina.a.yamanaka@rakuten.com>
2018-07-06 01:53:05 +0000

    Couponページの編集

---
+++ sasorich2/resources/views/welcome.blade.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Cool Layout with Complex Chainable Animation</title>
+  
+  
+  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+
+      <link rel="stylesheet" href="css/index.css">
+
+  
+</head>
+
+<body>
+
+  <div class="cont s--inactive">
+  <!-- cont inner start -->
+  <div class="cont__inner">
+    <!-- el start -->
+    <div class="el">
+      <div class="el__overflow">
+        <div class="el__inner">
+          <div class="el__bg"></div>
+          <div class="el__preview-cont">
+            <h2 class="el__heading">Coupon</h2>
+          </div>
+            <div class="el__content">
+              <div class="el__text">
+                <div class="el__title">Coupon Top5</div>
+                <ol>
+                  <li>Rakuten services</li>
+                  <li>Cafe & Restaurant around office</li>
+                  <li>Movie & Karaoke</li>
+                  <li>Amusement park & pool</li>
+                  <li>Shopping</li>
+                </ol></div>
+      <div class="el__index">
+        <div class="el__index-back">R</div>
+        <div class="el__index-front">
+          <div class="el__index-overlay" data-index="R">R</div>
+        </div>
+      </div>
+    </div>
+    <!-- el end -->
+    <!-- el start -->
+    <div class="el">
+      <div class="el__overflow">
+        <div class="el__inner">
+          <div class="el__bg"></div>
+          <div class="el__preview-cont">
+            <h2 class="el__heading">Section 2</h2>
+          </div>
+          <div class="el__content">
+            <div class="el__text">Whatever</div>
+            <div class="el__close-btn"></div>
+          </div>
+        </div>
+      </div>
+      <div class="el__index">
+        <div class="el__index-back">A</div>
+        <div class="el__index-front">
+          <div class="el__index-overlay" data-index="A">A</div>
+        </div>
+      </div>
+    </div>
+    <!-- el end -->
+    <!-- el start -->
+    <div class="el">
+      <div class="el__overflow">
+        <div class="el__inner">
+          <div class="el__bg"></div>
+          <div class="el__preview-cont">
+            <h2 class="el__heading">Section 3</h2>
+          </div>
+          <div class="el__content">
+            <div class="el__text">Whatever</div>
+            <div class="el__close-btn"></div>
+          </div>
+        </div>
+      </div>
+      <div class="el__index">
+        <div class="el__index-back">K</div>
+        <div class="el__index-front">
+          <div class="el__index-overlay" data-index="K">K</div>
+        </div>
+      </div>
+    </div>
+    <!-- el end -->
+    <!-- el start -->
+    <div class="el">
+      <div class="el__overflow">
+        <div class="el__inner">
+          <div class="el__bg"></div>
+          <div class="el__preview-cont">
+            <h2 class="el__heading">Section 4</h2>
+          </div>
+          <div class="el__content">
+            <div class="el__text">Whatever</div>
+            <div class="el__close-btn"></div>
+          </div>
+        </div>
+      </div>
+      <div class="el__index">
+        <div class="el__index-back">U</div>
+        <div class="el__index-front">
+          <div class="el__index-overlay" data-index="U">U</div>
+        </div>
+      </div>
+    </div>
+    <!-- el end -->
+    <!-- el start -->
+    <div class="el">
+      <div class="el__overflow">
+        <div class="el__inner">
+          <div class="el__bg"></div>
+          <div class="el__preview-cont">
+            <h2 class="el__heading">Section 5</h2>
+          </div>
+          <div class="el__content">
+            <div class="el__text">Whatever</div>
+            <div class="el__close-btn"></div>
+          </div>
+        </div>
+      </div>
+      <div class="el__index">
+        <div class="el__index-back">T</div>
+        <div class="el__index-front">
+          <div class="el__index-overlay" data-index="T">T</div>
+        </div>
+      </div>
+    </div>
+    <!-- el end -->
+        <!-- el start -->
+    <div class="el">
+      <div class="el__overflow">
+        <div class="el__inner">
+          <div class="el__bg"></div>
+          <div class="el__preview-cont">
+            <h2 class="el__heading">Section 6</h2>
+          </div>
+          <div class="el__content">
+            <div class="el__text">Whatever</div>
+            <div class="el__close-btn"></div>
+          </div>
+        </div>
+      </div>
+      <div class="el__index">
+        <div class="el__index-back">E</div>
+        <div class="el__index-front">
+          <div class="el__index-overlay" data-index="E">E</div>
+        </div>
+      </div>
+    </div>
+    <!-- el end -->
+    
+            <!-- el start -->
+    <div class="el">
+      <div class="el__overflow">
+        <div class="el__inner">
+          <div class="el__bg"></div>
+          <div class="el__preview-cont">
+            <h2 class="el__heading">Section 7</h2>
+          </div>
+          <div class="el__content">
+            <div class="el__text">Whatever</div>
+            <div class="el__close-btn"></div>
+          </div>
+        </div>
+      </div>
+      <div class="el__index">
+        <div class="el__index-back">N</div>
+        <div class="el__index-front">
+          <div class="el__index-overlay" data-index="N">N</div>
+        </div>
+      </div>
+    </div>
+    <!-- el end -->
+    
+  </div>
+  <!-- cont inner end -->
+</div>
+
+  
+  
+
+    <script  src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>
